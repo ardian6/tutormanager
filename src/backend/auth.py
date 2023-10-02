41835bf7331dcb68fe4fa229bd0cb60130595c49
Ardian Magjarraj <z5312076@ad.unsw.edu.au>
2023-10-02 15:05:30 +1100

    Added comments to function stubs

--- src/backend/auth.py
+++ src/backend/auth.py
@@ -1,5 +1,23 @@
 def login(username, password):
+  """User logs in.
+    Paramaters:
+      username: String
+      password: String
+    Returns:
+      session_token: String
+  """
   return
 
 def register(email, username, password, firstName, lastName, userType):
+  """User provides credentials to register.
+    Paramaters:
+      email: String
+      username: String
+      password: String
+      firstName: String
+      lastName: String
+      userType: String
+    Returns:
+      session_token: String
+  """
   return
--- src/backend/profile.py
+++ src/backend/profile.py
@@ -1,25 +1,94 @@
 def changeUsername(session_token, newUsername):
+  """Changes a given user's username.
+    Paramaters:
+      session_token: String
+      newUsername: String
+
+    Returns:
+      session_token: String
+  """
   return
 
 def changeEmail(session_token, newEmail):
+  """Changes a given user's email.
+    Paramaters:
+      session_token: String
+      newEmail: String
+    Returns:
+      session_token: String
+  """
   return
 
-def changePassword(session_token, newPassword)
+def changePassword(session_token, newPassword):
+  """Changes a given user's password.
+    Paramaters:
+      session_token: String
+      newPassword: String
+    Returns:
+      session_token: String
+  """
   return
 
 def changeBio(session_token, newBio):
-	return
-  
-def changeCurrentCourses(session_token, oldCourses, newCourses):
-	return
-
-def deleteAccount(session_token, password):
-	return
-
-def viewProfile(session_token, targetProfile)
+  """Changes a given user's bio.
+    Paramaters:
+      session_token: String
+      newBio: String
+    Returns:
+      session_token: String
+  """
   return
 
-def adminDelete(session_token, targetProfile)
+def addNewCourse(session_token, newCourse, newCourses):
+  """Adds a course to the given user's courses.
+    Paramaters:
+      session_token: String
+      oldCourses: String[]
+      newCourses: String[]
+    Returns:
+      session_token: String
+  """
+  return
+
+def deleteCourse(session_token, oldCourses, newCourses):
+  """Deletes a course from a given user.
+    Paramaters:
+      session_token: String
+      oldCourses: String[]
+      newCourses: String[]
+    Returns:
+      session_token: String
+  """
+  return
+
+def deleteAccount(session_token, password):
+  """Deletes the given user's account.
+    Paramaters:
+      session_token: String
+      password: String
+    Returns:
+      session_token: String
+  """
+  return
+
+def viewProfile(session_token, targetProfile):
+  """Current user views target user's profile.
+    Paramaters:
+      session_token: String
+      targetProfile: String
+    Returns:
+      session_token: String
+  """
+  return
+
+def adminDelete(session_token, targetProfile):
+  """Admin user deletes the target user.
+    Paramaters:
+      session_token: String
+      targetProfile: String
+    Returns:
+      session_token: String
+  """
   return
 
 # def uploadDocumentation(session_token)
