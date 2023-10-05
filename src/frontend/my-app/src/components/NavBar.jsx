d5ff3f90b2303bd2b249032316cbdaff7fcf69b8
Michael lu <z5310361@ad.unsw.edu.au>
2023-10-05 19:58:58 +1100

    Added side bar

--- src/frontend/my-app/src/components/NavBar.jsx
+++ src/frontend/my-app/src/components/NavBar.jsx
@@ -1,18 +1,20 @@
+import { Link } from "react-router-dom";
+import PositionedMenu from "./PositionedMenu";
+import SideBar from "./SideBar";
+
 function NavBar() {
     return (
         <div className="NavBarContainer">
             <div className="NavBar">
-                <div>
-                    <h5>
-                        About
-                    </h5>
+                <div className="NavBarInfo">
+                    <SideBar></SideBar>
+                    <div><Link to="/">About</Link></div>
+                    <div><Link to="/">Dashboard</Link></div>
                 </div>
                 <h4>
                     Tutor Manager
                 </h4>
-                <h5>
-                    Profile
-                </h5>
+                <PositionedMenu className="ProfileContainer">Profile</PositionedMenu>
             </div>
         </div>
     )
