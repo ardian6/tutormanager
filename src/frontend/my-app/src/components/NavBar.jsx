6620d30ab29694b3388a6c3c387a115ace534fb6
Michael lu <z5310361@ad.unsw.edu.au>
2023-10-05 16:11:28 +1100

    Played around with home page

---
+++ src/frontend/my-app/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+function NavBar() {
+    return (
+        <div className="NavBarContainer">
+            <div className="NavBar">
+                <div>
+                    <h5>
+                        About
+                    </h5>
+                </div>
+                <h4>
+                    Tutor Manager
+                </h4>
+                <h5>
+                    Profile
+                </h5>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
