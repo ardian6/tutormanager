34513e375dd6e125d1f6b80754a204a6a708f5b6
Michael lu <z5310361@ad.unsw.edu.au>
2023-10-09 16:03:15 +1100

    Connected login

--- src/frontend/my-app/src/pages/Login.jsx
+++ src/frontend/my-app/src/pages/Login.jsx
@@ -55,13 +55,7 @@ const Login = () => {
 
   // Login Button - calls backend API
   const loginBtn = async () => {
-<<<<<<< HEAD
-    const response = await fetch('http://localhost:5005/user/auth/login', {
-=======
-    console.log(email);
-    console.log(pwd);
     const response = await fetch('http://localhost:5005/auth/login/', {
->>>>>>> 243912bbd4e81a6ee8f471508354224eba1e1087
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
@@ -75,8 +69,7 @@ const Login = () => {
     if (data.error) {
       alert(data.error);
     } else {
-      // setToken(data.token);
-      // setEmailGlobal(email);
+      console.log(data);
     }
   }
 
