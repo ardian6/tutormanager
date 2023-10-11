e4d3ff28d8aed0175a14106eb4b0e71456f6a6ba
Ryan Gian <z5360294@ad.unsw.edu.au>
2023-10-11 16:26:33 +1100

    changing ui profile page

--- src/frontend/my-app/src/pages/Profile.jsx
+++ src/frontend/my-app/src/pages/Profile.jsx
@@ -11,9 +11,10 @@ import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import defaultImage from "./DefaultProfile.png";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import EditIcon from "@mui/icons-material/Edit";
 
 const Profile = () => {
-  const [userName, setUserName] = React.useState("zid");
+  const [userName, setUserName] = React.useState("MickyLuLu");
   const [email, setEmail] = React.useState("zid@ad.unsw.edu.au");
   const [firstName, setFirstName] = React.useState("Michael");
   const [lastName, setLastName] = React.useState("Lu");
@@ -35,20 +36,34 @@ const Profile = () => {
 
           <div className="profile-upper">
             <div className="upper-box-one">
+              <EditIcon className="edit-icon"></EditIcon>
               <img
                 src={defaultImage}
                 alt="default-image"
                 className="profile-image"
               />
             </div>
-            <div>
-              <div>Username: {userName}</div>
-              <div>Email: {email}</div>
-              <div>City: {city}</div>
-              <div>Bio: {bio}</div>
+            <div className="upper-box-two">
+              <EditIcon className="edit-icon"></EditIcon>
+              <div>
+                <b>Username:</b> {userName}
+              </div>
+              <div>
+                <b>Email:</b> {email}
+              </div>
+              <div>
+                <b>City:</b> {city}
+              </div>
+              <div>
+                <b>Subjects:</b> {subjects}
+              </div>
             </div>
-            <div>
-              <div>Subjects: {subjects}</div>
+            <div className="upper-box-three">
+              <EditIcon className="edit-icon"></EditIcon>
+              <div>
+                <b>Bio:</b>
+              </div>
+              <div> {bio}</div>
             </div>
           </div>
         </div>
