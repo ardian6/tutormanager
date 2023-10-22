dd3f721b1d533ba9aa25be364fdbbbfd88fa2df3
Michael lu <z5310361@ad.unsw.edu.au>
2023-10-22 20:56:27 +1100

    Added calendar to have hour by hour

--- src/frontend/my-app/src/components/Calendar.jsx
+++ src/frontend/my-app/src/components/Calendar.jsx
@@ -1,12 +1,53 @@
-import * as React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import React from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
 
-export default function BasicDateCalendar() {
+const localizer = momentLocalizer(moment);
+
+export default function BasicDateCalendar({token}) {
+  const [bookings, setBookings] = React.useState("");
+  const getBookings = async () => {
+    const response = await fetch(
+      "http://localhost:5005/bookings/view-my-bookings",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          token: token,
+        }),
+      }
+    );
+    const data = await response.json();
+    if (data.error) {
+      alert(data.error);
+    } else {
+      console.log(data.bookingsList)
+      const dates = data.bookingsList.map((element) => {
+        return {
+          start: moment(element[3]).toDate(),
+          end: moment(element[4]).toDate(),
+        }
+      });
+      setBookings(dates);
+    }
+  }
+  React.useEffect(() => {
+    getBookings();
+  }, []);
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
-    </LocalizationProvider>
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={bookings}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        // eventPropGetter={eventStyleGetter}
+      />
+    </div>
   );
 }
+
--- src/frontend/my-app/src/pages/StudentDashboard.jsx
+++ src/frontend/my-app/src/pages/StudentDashboard.jsx
@@ -30,27 +30,6 @@ const StudentDashboard = () => {
     }
   }
 
-  const getBookings = async () => {
-    const response = await fetch(
-      "http://localhost:5005/bookings/view-my-bookings",
-      {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify({
-          token: token,
-        }),
-      }
-    );
-    const data = await response.json();
-    if (data.error) {
-      alert(data.error);
-    } else {
-      console.log(data);
-    }
-  }
-
   const navigate = useNavigate()
   const redirectStudent = (id) => {
     const path = '/Profile/' + id
@@ -59,7 +38,6 @@ const StudentDashboard = () => {
 
   React.useEffect(() => {
     getAllStudents();
-    getBookings();
   }, []);
   return (
     <>
@@ -67,9 +45,8 @@ const StudentDashboard = () => {
       <div className="studentdashboard-container">
         <div className="studentdashboard-card">
           <div className="student-dashboard-title">Student Dashboard</div>
-
           <div className="student-calendar">
-            <Calendar></Calendar>
+            <Calendar token={token}></Calendar>
           </div>
           <div className="student-request-column">
             <div className="student-no-request-message">
