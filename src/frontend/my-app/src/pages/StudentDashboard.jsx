ce6e4d924a9a960892854c1ce5876faccea4f1f2
Ryan Gian <z5360294@ad.unsw.edu.au>
2023-10-22 19:27:34 +1100

    added searching for tutor

--- src/frontend/my-app/src/pages/StudentDashboard.jsx
+++ src/frontend/my-app/src/pages/StudentDashboard.jsx
@@ -3,7 +3,14 @@ import "./StudentDashboard.css";
 import NavBar from "../components/NavBar";
 import { Context, useContext } from "../Context";
 import { useNavigate } from "react-router-dom";
-
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TuneIcon from "@mui/icons-material/Tune";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 const StudentDashboard = () => {
   const { getters } = useContext(Context);
   const token = getters.token;
@@ -28,24 +35,29 @@ const StudentDashboard = () => {
     } else {
       setStudents(data.usersList);
     }
-  }
+  };
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const redirectStudent = (id) => {
-    const path = '/Profile/' + id
+    const path = "/Profile/" + id;
     navigate(path);
-  }
+  };
 
   React.useEffect(() => {
     getAllStudents();
   }, []);
+
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return (
     <>
       <NavBar></NavBar>
       <div className="studentdashboard-container">
         <div className="studentdashboard-card">
           <div className="student-dashboard-title">Student Dashboard</div>
-
           <div className="student-calendar">Calendar</div>
           <div className="student-request-column">
             <div className="student-no-request-message">
@@ -54,11 +66,43 @@ const StudentDashboard = () => {
                   <div key={idx} onClick={() => redirectStudent(student)}>
                     {student}
                   </div>
-                )
+                );
               })}
             </div>
             <div className="student-request-title">
               <div className="student-request-word">Tutor Requests</div>
+            </div>
+          </div>
+          <div className="search-container">
+            <div className="filters-container">
+              <Stack spacing={2} direction="row">
+                <Button variant="outlined">
+                  <TuneIcon className="filter-icon" />
+                  Filters
+                </Button>
+                {/* <div className="sort-container">Sort</div> */}
+              </Stack>
+            </div>
+
+            <div className="search-for-tutors-title">
+              <div className="search-for-tutors-word">Search For Tutors</div>
+            </div>
+            <div className="lower-box-container">
+              <div className="tutor-search-scroll">
+                {students.map((student, idx) => {
+                  return (
+                    // <div key={idx} onClick={() => redirectStudent(student)}>
+                    //   {student}
+                    // </div>
+                    <div
+                      key={idx}
+                      className="tutor-search-individual-container"
+                    >
+                      {student}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
